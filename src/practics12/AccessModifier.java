90dff50e24ddb3d23b06b58e4098edc58c7cbe5e
kasharaylo <kasharaylo@gmail.com>
2021-02-18 16:15:39 +0200

    Practice 09

---
+++ src/practics12/AccessModifier.java
@@ -0,0 +1,12 @@
+package practics12;
+
+public class AccessModifier {
+    public static void main (String args[]){
+        Employee100 empl1 = new Employee100();
+        //empl1.displayEmployeeName("Kevin"); //private
+
+        empl1.displayEmployeeId(212); //default
+        empl1.displayEmployeeSalary(500.40); //protected
+        empl1.displayEmployeeBenefits(100.400); //public
+    }
+}
